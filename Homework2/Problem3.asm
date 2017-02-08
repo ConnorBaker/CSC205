1d0f7a899168ff1e4ada8779d5ebb2a60d75f781
Connor Baker <connorbaker01@gmail.com>
2017-02-07 23:06:19 -0500

    Homework2: Problem3: v0.1b

--- Homework2/Problem3.asm
+++ Homework2/Problem3.asm
@@ -1,8 +1,8 @@
-#  Filename: 			DecimalFractionToBinaryFraction.java
+#  Filename: 			Problem3.asm
 #  Author: 				Connor Baker
-#  Version: 			0.1a
+#  Version: 			0.1b
 #  Date created:	February 5th, 2017
-#  Last modified: February 5th, 2017
+#  Last modified: February 7th, 2017
 #
 #  Description: 	MIPS translation of the mathematical statement
 #									A = (B - 210) * C.
@@ -11,35 +11,39 @@
 #									varB = 356, varC = 2.
 
 ################################### DATA ###################################
-	.data
+.data
 varA:			.word	0
 varB:			.word	356
 varC:			.word	2
-varD:			.word 210
 
-msg:			.asciiz "(356 - 210) * 2 ="
-msgadr:		.word	msg								# Store the address of string msg
-																	# Avoid the use of the "la" pseudo-instruction
+msg:			.asciiz "(356 - 210) * 2 = "
+msgadr:		.word	msg							# Store the address of string msg
+																# Avoid the use of the "la" pseudo-instruction
 
 ################################### TEXT ###################################
-	.text
-	.globl 	main										# must be global
+.text
+.globl 	main										# must be global
 main:
-	lw			$t1,varB								# Load varB
-	lw			$t2,varD								# Load varD
-	sub			$t0,$t1,$t2							# varB - varD
-	lw			$t1,varC								# Load varC
-	mult		$t0,$t1									# Multiply the difference of varB and varD by
-																	# varC.
+lw			$t0,varB								# Load varB into $t0
+addi		$t1,$t0,-210						# Calculate the difference of varB - 210 for $t1
+# We no longer need $t0. The next step uses $t0
+lw			$t0,varC								# Load varC in $t1 since we're done with varB
+mult		$t1,$t0									# Multiply the difference of varB and varD by
+																# varC and store in $t2
+# We no longer need $t0, $t1.
+mflo 		$s0											# Store the product in $s0
+sw			$s0,varA								# Set varA equal to the product
 
-	sw			$s0,zee									# set zee = aaa + bee
+# Code for printing out the result of the computation
+add			$v0,$zero,4							# Print the string function code
+# The number four is the code to trigger printing for a string for a syscall
+lw			$a0,msgadr							# Get the address of the string "msg" to print
+syscall													# Print the string "msg"
+add	$v0,$zero,1									# Print integer function code
+# The number one is the code to trigger printing for an integer for a syscall
+lw	$a0,varA										# Pass the product as an argument to print
+syscall													# Print the result of our computation
 
-	add			$v0,$zero,4		# print string function code
-	lw	$a0,msgadr		# get address of string "msg" to print
-	syscall				# print the string "msg"
-	add	$v0,$zero,1		# print integer function code
-	lw	$a0,zee			# get the integer to print
-	syscall				# print it!
-
-	add	$v0,$zero,10		# graceful exit function code
-	syscall				# exit program
+# Correctly exit the program
+add	$v0,$zero,10								# graceful exit function code
+syscall													# exit program
