330fe4d3a5002543ac6222a43ad87152c2d0038d
Connor Baker <connorbaker01@gmail.com>
2017-02-08 00:27:36 -0500

    Homework2: Versionless update

--- Homework2/Problem2.asm
+++ Homework2/Problem2.asm
@@ -17,21 +17,32 @@ main:
 
 
 begin:
-addi	$t5,$zero,0
-addi	$t6,$zero,1
-addi	$t1,$zero,1
+addi	$t5,$zero,0 # Store 0 in $t5
+addi	$t6,$zero,1 # Store 1 in $t6
+addi	$t1,$zero,1 # Store 1 in $t1
 
-loop:
-slt	$t2,$a0,$t1
-bne	$t2,$zero,finish
-add	$t7,$t5,$t6
-add	$t5,$zero,$t6
-add	$t6,$zero,$t7
-addi	$t1,$t1,1
-j	loop
+loop: # Start loop
+slt	$t2,$a0,$t1 # returns 1 if true, 0 if false. $a0 defaults to 1 in QtSPim.
+								#	As such, this returns 0.
+bne	$t2,$zero,finish # If $t2 is not equal to zero, terminate. However, $t2 is
+										 # one because of the above slt operation.
+add	$t7,$t5,$t6 # Add zero to one and store in $t7
+# $t7 is now one.
+add	$t5,$zero,$t6 # Add zero to one and store in $t5
+# $t5 is now one.
+add	$t6,$zero,$t7 # Add zero to one and store in $t6
+# $t6 is now one.
+addi	$t1,$t1,1 # Add one to one and store in $t1
+# $t1 is now two.
+j	loop # Loops back through again.
+# On this next iteration, the slt will return 1, which will cause the bne to
+# fall through because one is not equal to zero. As such, the loop will
+# terminate.
+
 
 finish:
-add	$v0,$t7,$zero
+add	$v0,$t7,$zero # $t7 was only touched once, so it's still one. One plus zero
+# is one, which is stored in $v0.
 
 # Find out what $v0 contains
 sw $v0,varA
