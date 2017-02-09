044c72e747844877410181a5a237ef046b8cdec9
Connor Baker <connorbaker01@gmail.com>
2017-02-09 10:33:48 -0500

    Homework2: Problem4: v0.1b

--- Homework2/Problem4.asm
+++ Homework2/Problem4.asm
@@ -1,8 +1,8 @@
 #  Filename: 			Problem4.asm
 #  Author: 				Connor Baker
-#  Version: 			0.1a
+#  Version: 			0.1b
 #  Date created:	February 8th, 2017
-#  Last modified: February 8th, 2017
+#  Last modified: February 9th, 2017
 #
 #  Description: 	MIPS translation of the C statement
 #	                if  (q == r)
@@ -11,7 +11,10 @@
 #                   q = r - q;
 #                 Where q (varQ) is 5 and r (varR) is 3
 
-################################### DATA ###################################
+
+
+#################################### DATA ####################################
+# THIS IS THE DATA SECTION OF THE ASM PROGRAM
 .data
 varQ: .word 5
 varR:	.word	3
@@ -23,7 +26,10 @@ msg2: .asciiz "\nr is "
 msgadr2: .word	msg2 # Store the address of string msg
 # Avoid the use of the "la" pseudo-instruction
 
-################################### TEXT ###################################
+
+
+#################################### TEXT ####################################
+# THIS IS THE TEXT SECTION OF THE PROGRAM
 .text
 .globl main # must be global
 main:
@@ -42,7 +48,10 @@ sw $s0,varQ # Update the value of varQ
 
 endif: # Do nothing
 
-# Code for printing out the result of the computation
+
+
+################################## PRINTING ##################################
+# THIS IS THE SECTION OF THE PROGRAM THAT PRINTS OUTPUT TO ENSURE CORRECTNESS
 # Print out q
 add	$v0,$zero,4 # Print the string function code
 # The number four is the code to trigger printing for a string for a syscall
