c25b969a798b87b2d5cd3612883fda4b0035c71e
Connor Baker <connorbaker01@gmail.com>
2017-02-09 11:54:38 -0500

    Homework2: Problem2: v0.1c

--- Homework2/Problem2.asm
+++ Homework2/Problem2.asm
@@ -1,52 +1,51 @@
 #  Filename: 			Problem2.asm
 #  Author: 				Connor Baker
-#  Version: 			0.1b
+#  Version: 			0.1c
 #  Date created:	February 7th, 2017
 #  Last modified: February 9th, 2017
 #
-#  Description: 	Adding comments to the following program
+#  Description: 	This program calculates (given an positive arugment for $a0)
+#                 the (n-1)th element of the Fibonacci sequence (in this case,
+#                 just the later portion of it: the set is zero-indexed and
+#                 contains {1,2,3,5,...}).
 
 
 
 #################################### DATA ####################################
 # THIS IS THE DATA SECTION OF THE ASM PROGRAM
 .data
-varA:			.word	0
+varA:			.word	0 # Dummy variable to hold the calculated value
 
 
 
 #################################### TEXT ####################################
 # THIS IS THE TEXT SECTION OF THE PROGRAM
 .text
-.globl 	main										# must be global
+.globl 	main # must be global
 main:
 
 begin:
-addi	$t5,$zero,0 # Store 0 in $t5
-addi	$t6,$zero,1 # Store 1 in $t6
-addi	$t1,$zero,1 # Store 1 in $t1
+addi	$t5,$zero,0 # Store F_0 = 0 in $t5
+addi	$t6,$zero,1 # Store F_1 = 1 in $t6
+addi	$t1,$zero,1 # Store 1 in $t1, which is our iteration counter
+
 
 loop: # Start loop
-slt	$t2,$a0,$t1 # returns 1 if true, 0 if false. $a0 defaults to 1 in QtSPim.
-								#	As such, this returns 0.
-bne	$t2,$zero,finish # If $t2 is not equal to zero, terminate. However, $t2 is
-										 # one because of the above slt operation.
-add	$t7,$t5,$t6 # Add zero to one and store in $t7
-# $t7 is now one.
-add	$t5,$zero,$t6 # Add zero to one and store in $t5
-# $t5 is now one.
-add	$t6,$zero,$t7 # Add zero to one and store in $t6
-# $t6 is now one.
-addi	$t1,$t1,1 # Add one to one and store in $t1
-# $t1 is now two.
+slt	$t2,$a0,$t1 # If we've iterated enough time to arrive at the specified
+                # Fibonacci number, set $t2 = 1 so that the bne below
+                # terminates the program. Otherwise, set $t2 = 0.
+bne	$t2,$zero,finish # If $t2 is not equal to zero, terminate.
+add	$t7,$t5,$t6 # Set F_n = F_{n-2} + F_{n-1}
+# $t7 is now the nth Fibonacci number, where n is the iteration number.
+add	$t5,$zero,$t6 # Update F_{n-2} since we have a new F_n.
+# $t5 is now the (n-2)th Fibonacci number, where n is the iteration number.
+add	$t6,$zero,$t7 # Update F_{n-1} since we have a new F_n.
+# $t6 is now the (n-1)th Fibonacci number, where n is the iteration number.
+addi	$t1,$t1,1 # Update our iteration counter.
 j	loop # Loops back through again.
-# On this next iteration, the slt will return 1, which will cause the bne to
-# fall through because one is not equal to zero. As such, the loop will
-# terminate.
 
 finish:
-add	$v0,$t7,$zero # $t7 was only touched once, so it's still one. One plus zero
-# is one, which is stored in $v0.
+add	$v0,$t7,$zero # Now that we've found the (n-1)th element of the Fibonacci sequence, store it in $v0.
 
 # Find out what $v0 contains
 sw $v0,varA
